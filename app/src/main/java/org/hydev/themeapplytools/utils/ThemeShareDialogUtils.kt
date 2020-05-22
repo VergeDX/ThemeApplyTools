329f30c9a4b99c4d0e8ae0d7a2bcc9dc6828816c
Hykilpikonna <admin@moecraft.cc>
2020-05-22 16:40:06 -0400

    [O] Shorten ThemeSharDialogUtils code

--- app/src/main/java/org/hydev/themeapplytools/utils/ThemeShareDialogUtils.kt
+++ app/src/main/java/org/hydev/themeapplytools/utils/ThemeShareDialogUtils.kt
@@ -3,24 +3,23 @@ package org.hydev.themeapplytools.utils
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import android.view.View
 import org.hydev.themeapplytools.databinding.DialogThemeShareBinding
 
 object ThemeShareDialogUtils
 {
-    private const val THEME_OFFICIAL_URL = "http://zhuti.xiaomi.com/"
-    private const val THEMES_TK_URL = "https://miuithemes.tk/"
-    private const val TECH_RUCHI_URL = "http://www.techrushi.com/"
-    private const val THEME_XIAOMIS_URL = "https://miuithemesxiaomis.blogspot.com/"
-    private const val XIAOMI_THEMEZ_URL = "https://www.miuithemez.com/"
+    const val THEME_OFFICIAL_URL = "http://zhuti.xiaomi.com/"
+    const val THEMES_TK_URL = "https://miuithemes.tk/"
+    const val TECH_RUCHI_URL = "http://www.techrushi.com/"
+    const val THEME_XIAOMIS_URL = "https://miuithemesxiaomis.blogspot.com/"
+    const val XIAOMI_THEMEZ_URL = "https://www.miuithemez.com/"
 
     fun setOnClickListener(activity: Activity, dialogThemeShareBinding: DialogThemeShareBinding)
     {
-        dialogThemeShareBinding.btOfficialStore.setOnClickListener { v: View? -> openBrowser(activity, THEME_OFFICIAL_URL) }
-        dialogThemeShareBinding.btThemesTK.setOnClickListener { v: View? -> openBrowser(activity, THEMES_TK_URL) }
-        dialogThemeShareBinding.btTechruchi.setOnClickListener { v: View? -> openBrowser(activity, TECH_RUCHI_URL) }
-        dialogThemeShareBinding.btThemeXiaomis.setOnClickListener { v: View? -> openBrowser(activity, THEME_XIAOMIS_URL) }
-        dialogThemeShareBinding.btThemez.setOnClickListener { v: View? -> openBrowser(activity, XIAOMI_THEMEZ_URL) }
+        dialogThemeShareBinding.btOfficialStore.setOnClickListener { openBrowser(activity, THEME_OFFICIAL_URL) }
+        dialogThemeShareBinding.btThemesTK.setOnClickListener { openBrowser(activity, THEMES_TK_URL) }
+        dialogThemeShareBinding.btTechruchi.setOnClickListener { openBrowser(activity, TECH_RUCHI_URL) }
+        dialogThemeShareBinding.btThemeXiaomis.setOnClickListener { openBrowser(activity, THEME_XIAOMIS_URL) }
+        dialogThemeShareBinding.btThemez.setOnClickListener { openBrowser(activity, XIAOMI_THEMEZ_URL) }
     }
 
     fun openBrowser(activity: Activity, URL: String?)
